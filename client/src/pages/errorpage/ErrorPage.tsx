91aa458dbc9cc8659815a37d7255759479630c32
GARY GRAS <gary.gras07@gmail.com>
2025-07-24 19:22:56 +0200

    feat: creation of about page and error page

--- client/src/pages/errorpage/ErrorPage.tsx
+++ client/src/pages/errorpage/ErrorPage.tsx
@@ -2,27 +2,27 @@ import { useEffect } from "react";
 import "./errorPage.css";
 
 export default function ErrorPage() {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      window.location.href = "/profil";
-    }, 5000);
-    return () => clearTimeout(timer);
-  }, []);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            window.location.href = "/profil";
+        }, 5000);
+        return () => clearTimeout(timer);
+    }, []);
 
-  return (
-    <div className="error-container">
-      <div className="error-images">
-        <img
-          src="/src/assets/image/mobil.png"
-          alt="Erreur 404 mobile"
-          className="error-img error-img--mobile"
-        />
-        <img
-          src="/src/assets/image/desktop.png"
-          alt="Erreur 404 desktop"
-          className="error-img error-img--desktop"
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div className="error-container">
+            <div className="error-images">
+                <img
+                    src="/src/assets/image/mobil.png"
+                    alt="Erreur 404 mobile"
+                    className="error-img error-img--mobile"
+                />
+                <img
+                    src="/src/assets/image/desktop.png"
+                    alt="Erreur 404 desktop"
+                    className="error-img error-img--desktop"
+                />
+            </div>
+        </div>
+    );
 }
